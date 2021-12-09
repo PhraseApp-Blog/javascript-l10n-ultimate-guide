850c97b5a003aaf640ea99230968ddffe6b658bd
Mohammad Ashour <ashour.mohammad@gmail.com>
2021-12-09 17:23:10 +0200

    Adding basic translations

--- polyglot/src/index.js
+++ polyglot/src/index.js
@@ -1,3 +1,13 @@
 import Polyglot from "node-polyglot";
 
-console.log({ Polyglot });
+const polyglot = new Polyglot();
+
+polyglot.extend({
+  "app-title": "مع بوليجلوت",
+});
+
+const element = document.querySelector(
+  "[data-i18n-key='app-title']",
+);
+
+element.innerHTML = polyglot.t("app-title");
