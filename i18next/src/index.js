282a80125960ee395622760144de72dbb425d20b
Mohammad Ashour <ashour.mohammad@gmail.com>
2021-12-20 11:35:47 +0200

    Adding automatic user locale detection

--- i18next/src/index.js
+++ i18next/src/index.js
@@ -1,16 +1,20 @@
 import i18next from "i18next";
 import HttpApi from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 async function initI18next() {
-  await i18next.use(HttpApi).init({
-    lng: "ar",
-    debug: true,
-    supportedLngs: ["en", "ar"],
-    fallbackLng: "en",
-    backend: {
-      loadPath: "/lang/{{lng}}.json",
-    },
-  });
+  await i18next
+    .use(HttpApi)
+    .use(LanguageDetector)
+    .init({
+      debug: true,
+      supportedLngs: ["en", "ar"],
+      fallbackLng: "en",
+      nonExplicitSupportedLngs: true,
+      backend: {
+        loadPath: "/lang/{{lng}}.json",
+      },
+    });
 }
 
 function translatePageElements() {
