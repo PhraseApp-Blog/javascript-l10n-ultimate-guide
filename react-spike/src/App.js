34fc42672f32d9d594bb83fab0121d4a612b8124
Mohammad Ashour <ashour.mohammad@gmail.com>
2021-12-21 14:06:09 +0200

    Updating html[lang] and html[dir] on locale change

--- react-spike/src/App.js
+++ react-spike/src/App.js
@@ -1,9 +1,15 @@
+import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import Navbar from "./layout/Navbar";
 import "./App.css";
 
 function App() {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+
+  useEffect(() => {
+    document.documentElement.lang = i18n.language;
+    document.documentElement.dir = i18n.dir(i18n.language);
+  }, [i18n, i18n.language]);
 
   return (
     <div className="container">
