345d5247dd3a298eedc9e38c612f646dfd90eaad
Mohammad Ashour <ashour.mohammad@gmail.com>
2021-12-26 16:54:46 +0200

    Adding interpolation

--- jquery/js/scripts.js
+++ jquery/js/scripts.js
@@ -1,7 +1,7 @@
 async function initI18n() {
   await i18next.use(i18nextHttpBackend).init({ lng: "en" });
 
-  jqueryI18next.init(i18next, $);
+  jqueryI18next.init(i18next, $, { useOptionsAttr: true });
 }
 
 function translatePage() {
